7933dc5a305ed972c759762acb7f1331f2b15f6d
Fernando <fernando-chan@MacBook-Pro-de-Fernando.local>
2017-01-31 12:38:05 -0300

    Initial commit

---
+++ Example/Pods/Target Support Files/MicroKonex/MicroKonex-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_MicroKonex : NSObject
+@end
+@implementation PodsDummy_MicroKonex
+@end
---
+++ Example/Pods/Target Support Files/Pods-MicroKonex_Tests/Pods-MicroKonex_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MicroKonex_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_MicroKonex_Tests
+@end
